956ef9c59f10232ae71c0879dccb190bf4f51c48
Dmitry Sviridov <d.sviridov.qa@gmail.com>
2020-10-01 11:00:32 +0300

    remove hw_1 from master

--- HomeWork_1/app/src/main/java/ru/sviridov/fintech/homework/lesson3/dto/Contact.kt
+++
@@ -1,7 +0,0 @@
-package ru.sviridov.fintech.homework.lesson3.dto
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class Contact(val id: String, val name: String?, val phoneNum: String?) : Parcelable
