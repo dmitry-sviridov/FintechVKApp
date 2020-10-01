5501b01851a8b7b1a3b2d1911b98a413e73134d0
Dmitry Sviridov <d.sviridov.qa@gmail.com>
2020-10-01 10:45:50 +0300

    remove hw1 from hw2

--- HomeWork_1/app/src/main/java/ru/sviridov/fintech/homework/lesson3/common/Values.kt
+++
@@ -1,9 +0,0 @@
-package ru.sviridov.fintech.homework.lesson3.common
-
-// region Action names
-const val SEND_BROADCAST_CONTACTS_LIST = "action/send_broadcast_contacts_list"
-// endregion
-
-// region EXTRA names
-const val EXTRA_CONTACTS = "extra/contacts"
-// endregion
